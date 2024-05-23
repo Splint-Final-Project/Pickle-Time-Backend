07ed317b806aebd6f713237abf27478e6099d711
이유구 <leeyookoo@naver.com>
2024-05-23 17:02:11 +0900

    [feat] 초기환경세팅

---
+++ src/main/java/peakle_time/peakle_time/PeakleTimeApplication.java
@@ -0,0 +1,13 @@
+package peakle_time.peakle_time;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeakleTimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeakleTimeApplication.class, args);
+	}
+
+}
