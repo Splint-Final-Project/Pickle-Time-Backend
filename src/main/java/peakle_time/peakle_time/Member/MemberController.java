9a5f4a51a5e9ed6b8130975da611244fca5a21ed
yukudaa <leeyookoo@naver.com>
2024-05-24 15:38:03 +0900

    [feat] 초기환경설정

--- src/main/java/peakle_time/peakle_time/Member/MemberController.java
+++ src/main/java/peakle_time/peakle_time/Member/MemberController.java
@@ -1,19 +1,18 @@
 package peakle_time.peakle_time.Member;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import peakle_time.peakle_time.Member.dto.JoinRequest;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+
+@RestController
 @RequiredArgsConstructor
 public class MemberController {
 
     private final MemberService memberService;
 
-    @PostMapping("/user")
-    public String join(JoinRequest joinRequest) {
-        memberService.save(joinRequest);
-        return "redirect:/login";
-    }
+//    @PostMapping("/user")
+//    public String join(JoinRequest joinRequest) {
+//        memberService.save(joinRequest);
+//        return "redirect:/login";
+//    }
 }
--- src/main/java/peakle_time/peakle_time/Study/StudyController.java
+++ src/main/java/peakle_time/peakle_time/Study/StudyController.java
@@ -1,7 +1,7 @@
 package peakle_time.peakle_time.Study;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class StudyController {
 }
