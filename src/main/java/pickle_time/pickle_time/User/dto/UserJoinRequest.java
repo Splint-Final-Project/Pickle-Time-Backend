8d74836d212a580fa5a335d55220af903576efa4
yukudaa <leeyookoo@naver.com>
2024-05-27 23:53:00 +0900

    [feat] login

--- src/main/java/pickle_time/pickle_time/User/dto/UserJoinRequest.java
+++ src/main/java/pickle_time/pickle_time/User/dto/UserJoinRequest.java
@@ -1,11 +1,14 @@
 package pickle_time.pickle_time.User.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
 public record UserJoinRequest(
 
-        String email,
-        String password,
-        String checkPassword,
-        String nickname,
+        @Email(message = "이메일 형식대로 입력해 주세요") String email,
+        @NotBlank(message = "비밀번호를 입력해 주세요") String password,
+        @NotBlank(message = "비밀번호를 입력해 주세요") String checkPassword,
+        @NotBlank(message = "닉네임을 입력해 주세요") String nickname,
         String company,
         String imageUrl
 
