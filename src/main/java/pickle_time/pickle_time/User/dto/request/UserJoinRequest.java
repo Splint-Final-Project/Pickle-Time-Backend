44b5f60bcfaad41f94b7c1e160eddfab50f331cc
whai2 <whai2125@gmail.com>
2024-05-30 17:28:26 +0900

    fix: 유저 정보 일부 주석처리

--- src/main/java/pickle_time/pickle_time/User/dto/request/UserJoinRequest.java
+++ src/main/java/pickle_time/pickle_time/User/dto/request/UserJoinRequest.java
@@ -8,7 +8,7 @@ public record UserJoinRequest(
         @Email(message = "이메일 형식대로 입력해 주세요") String email,
         @NotBlank(message = "비밀번호를 입력해 주세요") String password,
         @NotBlank(message = "비밀번호를 입력해 주세요") String checkPassword,
-        @NotBlank(message = "닉네임을 입력해 주세요") String nickname,
+//        @NotBlank(message = "닉네임을 입력해 주세요") String nickname,
         String company,
         String imageUrl
 
